5ec759021328f14e1f85bb7e5c8f7d1fffd5e7f3
Mark Brown II <mbrownii@doc.gov>
2017-02-21 08:55:04 -0500

    Updates presentation

---
+++ CodeForClass/ReviewMaterials/script.js
@@ -0,0 +1,11 @@
+var destinyAlbums = ['Destiny\'s Child','The Writing\'s on the Wall', 'Survivor', 'Destiny Fulfilled', '8 Days of Christmas'];
+
+var whitneyAlbums = {
+	'Whitney Houston' :'Greatest Love of All',
+	'Whitney' : 'I Wanna Dance With Somebody',
+	'I\'m Your Baby Tonight' : 'All the Man That I Need',
+	'My Love Is Your Love': 'Heartbreak Hotel',
+	'The Bodyguard': 'I Will Always Love You',
+	'Just Whitney' : 'Try It On My Own',
+	'The Preacher\'s Wife': 'Joy'
+}
